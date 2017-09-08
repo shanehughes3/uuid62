b2a51f68e6930c03ebe3e5b1ad6954862ad07439
Shane Hughes <shanehughes314@gmail.com>
2017-09-08 13:32:48 -0500

    formatting

--- test/uuid-base62.test.js
+++ test/uuid-base62.test.js
@@ -2,98 +2,98 @@ var assert = require('assert');
 var uuidBase62 = require('../uuid-base62');
 
 describe('uuid-base62', function () {
-  describe('v4', function () {
-    it('should generate a unique id without any params', function () {
-      var res = uuidBase62.v4();
-      assert(res);
-      assert.equal(typeof res, 'string');
-      assert.equal(res.length, 22);
-    });
+	describe('v4', function () {
+		it('should generate a unique id without any params', function () {
+			var res = uuidBase62.v4();
+			assert(res);
+			assert.equal(typeof res, 'string');
+			assert.equal(res.length, 22);
+		});
 
-    it('should convert id back to uuid format', function () {
-      var uuid = uuidBase62.v4();
-      assert(uuid);
+		it('should convert id back to uuid format', function () {
+			var uuid = uuidBase62.v4();
+			assert(uuid);
 
-      var res = uuidBase62.decode(uuid);
+			var res = uuidBase62.decode(uuid);
 
-      assert.equal(typeof res, 'string');
-      assert.equal(res.length, 36);
+			assert.equal(typeof res, 'string');
+			assert.equal(res.length, 36);
 
-      var parts = res.split('-');
-      assert.equal(parts.length, 5);
-      parts.forEach(function (group) {
-        var num = parseInt(group, 16);
-        assert(num > 0);
-      });
-    });
+			var parts = res.split('-');
+			assert.equal(parts.length, 5);
+			parts.forEach(function (group) {
+				var num = parseInt(group, 16);
+				assert(num > 0);
+			});
+		});
 
-    it('should generate same encoded uuid from different formats', function () {
-      var uuid = 'de305d54-75b4-431b-adb2-eb6b9e546014';
-      var uuidB = new Buffer([0xde, 0x30, 0x5d, 0x54, 0x75, 0xb4, 0x43, 0x1b, 0xad, 0xb2, 0xeb, 0x6b, 0x9e, 0x54, 0x60, 0x14]);
+		it('should generate same encoded uuid from different formats', function () {
+			var uuid = 'de305d54-75b4-431b-adb2-eb6b9e546014';
+			var uuidB = new Buffer([0xde, 0x30, 0x5d, 0x54, 0x75, 0xb4, 0x43, 0x1b, 0xad, 0xb2, 0xeb, 0x6b, 0x9e, 0x54, 0x60, 0x14]);
 
-      var res = uuidBase62.encode(uuid);
-      var resB = uuidBase62.encode(uuidB);
+			var res = uuidBase62.encode(uuid);
+			var resB = uuidBase62.encode(uuidB);
 
-      assert.equal(resB, res);
-    });
+			assert.equal(resB, res);
+		});
 
-    it('decoded UUID should match original UUID', function () {
-      var uuid = 'de305d54-75b4-431b-adb2-eb6b9e546014';
+		it('decoded UUID should match original UUID', function () {
+			var uuid = 'de305d54-75b4-431b-adb2-eb6b9e546014';
 
-      var uuidB62 = uuidBase62.encode(uuid);
-      var res = uuidBase62.decode(uuidB62);
+			var uuidB62 = uuidBase62.encode(uuid);
+			var res = uuidBase62.decode(uuidB62);
 
-      assert.equal(res, uuid);
-    });
-  });
-  
-  describe('v1', function () {
-    it('should generate a unique id without any params', function () {
-      var res = uuidBase62.v1();
-      assert(res);
-      assert.equal(typeof res, 'string');
-      assert.equal(res.length, 22);
-    });
-  });
-  
-  describe('encode / decode', function () {
-    var fixtures = {
-      '0000000000000000000000': '00000000-0000-0000-0000-000000000000',
-      '0cBaidlJ84Ggc5JA7IYCgv': '06ad547f-fe02-477b-9473-f7977e4d5e17',
-      '4vqyd6OoARXqj9nRUNhtLQ': '941532a0-6be1-443a-a9d5-d57bdf180a52',
-      '5FY8KwTsQaUJ2KzHJGetfE': 'ba86b8f0-6fdf-4944-87a0-8a491a19490e',      
-      '7N42dgm5tFLK9N8MT7fHC7': 'ffffffff-ffff-ffff-ffff-ffffffffffff'
-    };
-    
-    Object.keys(fixtures).forEach(function(key){
-      it('should properly encode ' + fixtures[key], function () {
-        assert.equal(uuidBase62.encode(fixtures[key]), key);
-      });
-      
-      it('should properly decode ' + key, function () {
-        assert.equal(uuidBase62.decode(key), fixtures[key]);
-      });
-    });
-  });
-  
-  describe('other bases', function () {
-    it('should generate a unique id without any params in base64', function () {
-      uuidBase62.customBase = new uuidBase62.baseX("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_");
-      var res = uuidBase62.v4();
-      assert(res);
-      assert.equal(typeof res, 'string');
-      assert.equal(res.length, 22);
-    });
+			assert.equal(res, uuid);
+		});
+	});
 
-    it('should encode and decode a uuid in Base64', function () {
-      uuidBase62.customBase = new uuidBase62.baseX("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_");
-      var uuid = '72be7291-fbf6-400f-87c4-455e23d01cd5';
+	describe('v1', function () {
+		it('should generate a unique id without any params', function () {
+			var res = uuidBase62.v1();
+			assert(res);
+			assert.equal(typeof res, 'string');
+			assert.equal(res.length, 22);
+		});
+	});
 
-      var uuidB64 = uuidBase62.encode(uuid);
-      assert.equal(uuidB64, '1OLDah-_p03Uv4hlUzQ1Pl');
-      
-      var res = uuidBase62.decode(uuidB64);
-      assert.equal(res, uuid);
-    });
-  });
+	describe('encode / decode', function () {
+		var fixtures = {
+			'0000000000000000000000': '00000000-0000-0000-0000-000000000000',
+			'0cBaidlJ84Ggc5JA7IYCgv': '06ad547f-fe02-477b-9473-f7977e4d5e17',
+			'4vqyd6OoARXqj9nRUNhtLQ': '941532a0-6be1-443a-a9d5-d57bdf180a52',
+			'5FY8KwTsQaUJ2KzHJGetfE': 'ba86b8f0-6fdf-4944-87a0-8a491a19490e',
+			'7N42dgm5tFLK9N8MT7fHC7': 'ffffffff-ffff-ffff-ffff-ffffffffffff'
+		};
+
+		Object.keys(fixtures).forEach(function(key){
+			it('should properly encode ' + fixtures[key], function () {
+				assert.equal(uuidBase62.encode(fixtures[key]), key);
+			});
+
+			it('should properly decode ' + key, function () {
+				assert.equal(uuidBase62.decode(key), fixtures[key]);
+			});
+		});
+	});
+
+	describe('other bases', function () {
+		it('should generate a unique id without any params in base64', function () {
+			uuidBase62.customBase = new uuidBase62.baseX("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_");
+			var res = uuidBase62.v4();
+			assert(res);
+			assert.equal(typeof res, 'string');
+			assert.equal(res.length, 22);
+		});
+
+		it('should encode and decode a uuid in Base64', function () {
+			uuidBase62.customBase = new uuidBase62.baseX("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_");
+			var uuid = '72be7291-fbf6-400f-87c4-455e23d01cd5';
+
+			var uuidB64 = uuidBase62.encode(uuid);
+			assert.equal(uuidB64, '1OLDah-_p03Uv4hlUzQ1Pl');
+
+			var res = uuidBase62.decode(uuidB64);
+			assert.equal(res, uuid);
+		});
+	});
 });
--- uuid-base62.js
+++ uuid-base62.js
@@ -13,110 +13,110 @@ module.exports.length = 22;
 module.exports.uuidLength = 32;
 
 /**
- * v4
- */
+* v4
+*/
 module.exports.v4 = function v4() {
-  var self = this;
-  var args = Array.prototype.slice.call(arguments);
+	var self = this;
+	var args = Array.prototype.slice.call(arguments);
 
-  if (!args[1]) {
-    // make sure we use a buffer to avoid getting an uuid with dashes
-    args[1] = new Buffer(16);
-  }
+	if (!args[1]) {
+		// make sure we use a buffer to avoid getting an uuid with dashes
+		args[1] = new Buffer(16);
+	}
 
-  var id = uuid.v4.apply(self, args);
-  return self.encode(id);
+	var id = uuid.v4.apply(self, args);
+	return self.encode(id);
 };
 
 
 /**
- * v1
- */
+* v1
+*/
 module.exports.v1 = function v1() {
-  var self = this;
-  var args = Array.prototype.slice.call(arguments);
+	var self = this;
+	var args = Array.prototype.slice.call(arguments);
 
-  if (!args[1]) {
-    // make sure we use a buffer to avoid getting an uuid with dashes
-    args[1] = new Buffer(16);
-  }
+	if (!args[1]) {
+		// make sure we use a buffer to avoid getting an uuid with dashes
+		args[1] = new Buffer(16);
+	}
 
-  var id = uuid.v1.apply(self, args);
-  return self.encode(id);
+	var id = uuid.v1.apply(self, args);
+	return self.encode(id);
 };
 
 
 /**
- * encode
- */
+* encode
+*/
 module.exports.encode = function encode(input, encoding) {
-  encoding = encoding || 'hex';
-  
-  if (typeof input === 'string') {
-    // remove the dashes to save some space
-    input = new Buffer(input.replace(/-/g, ''), encoding);
-  }
-  return ensureLength(this.customBase.encode(input), module.exports.length);
+	encoding = encoding || 'hex';
+
+	if (typeof input === 'string') {
+		// remove the dashes to save some space
+		input = new Buffer(input.replace(/-/g, ''), encoding);
+	}
+	return ensureLength(this.customBase.encode(input), module.exports.length);
 };
 
 /**
- * decode
- */
+* decode
+*/
 module.exports.decode = function decode(b62Str, encoding) {
-  encoding = encoding || 'hex';
-  var res = ensureLength(new Buffer(this.customBase.decode(b62Str)).toString(encoding), module.exports.uuidLength);
-  
-  // re-add the dashes so the result looks like an uuid
-  var resArray = res.split('');
-  [8, 13, 18, 23].forEach(function(idx){
-    resArray.splice(idx, 0, '-');
-  });
-  res = resArray.join('');
+	encoding = encoding || 'hex';
+	var res = ensureLength(new Buffer(this.customBase.decode(b62Str)).toString(encoding), module.exports.uuidLength);
 
-  return res;
+	// re-add the dashes so the result looks like an uuid
+	var resArray = res.split('');
+	[8, 13, 18, 23].forEach(function(idx){
+		resArray.splice(idx, 0, '-');
+	});
+	res = resArray.join('');
+
+	return res;
 };
 
 /**
- * ensureLength
- * 
- * @api private
- */
+* ensureLength
+*
+* @api private
+*/
 function ensureLength(str, maxLen){
-  str = str + "";
-  if(str.length < maxLen){
-    return padLeft(str, maxLen);
-  }
-  else if (str.length > maxLen){
-    return trimLeft(str, maxLen);
-  }
-  return str;
+	str = str + "";
+	if(str.length < maxLen){
+		return padLeft(str, maxLen);
+	}
+	else if (str.length > maxLen){
+		return trimLeft(str, maxLen);
+	}
+	return str;
 };
 
 
 /**
- * padLeft
- * 
- * @api private
- */
+* padLeft
+*
+* @api private
+*/
 function padLeft(str, padding){
-  str = str + "";
-  var pad = "";
-  for(var i=str.length; i<padding; i++){
-    pad += '0';
-  }
-  return pad + str;
+	str = str + "";
+	var pad = "";
+	for(var i=str.length; i<padding; i++){
+		pad += '0';
+	}
+	return pad + str;
 };
 
 /**
- * trimLeft
- * 
- * @api private
- */
+* trimLeft
+*
+* @api private
+*/
 function trimLeft(str, maxLen){
-  str = str + "";
-  var trim = 0;
-  while(str[trim] === '0' && (str.length - trim) > maxLen){
-    trim++;
-  }
-  return str.slice(trim);
+	str = str + "";
+	var trim = 0;
+	while(str[trim] === '0' && (str.length - trim) > maxLen){
+		trim++;
+	}
+	return str.slice(trim);
 };
